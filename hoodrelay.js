198c7373e43c403934373f4a3b3f40734792e51d
Steve Teixeira <steve@stevetex.com>
2019-01-11 14:55:47 -0800

    initial checkiN

---
+++ hoodrelay.js
@@ -0,0 +1,71 @@
+const gpio = require('onoff').Gpio;
+const express = require('express');
+var app = express();
+
+const light = new gpio(7, 'out');
+const lofan = new gpio(3, 'out');
+const medfan = new gpio(22, 'out');
+const hifan = new gpio(25, 'out');
+
+function dolight(turnon) {
+    console.log('setting light to %s', turnon)
+    if (turnon) {
+        light.write(1);
+    } else {
+        light.write(0);
+    }
+}
+
+function dofan(turnon, speed) {
+    lofan.write(0);
+    medfan.write(0);
+    hifan.write(0);
+    if (turnon) {
+        console.log('setting fan to speed %d', speed)
+        switch(speed) {
+            case 1: lofan.write(1); break;
+            case 2: medfan.write(1); break;
+            case 3: hifan.write(1); break;
+        }
+    } else {
+        console.log('turned fan off')
+    }
+}
+
+app.post('/light', function (req, res) {
+    console.log('received POST: %s', req.query)
+    var val = req.query.value;
+    if (val === "on") {
+        dolight(true);
+    } else if (val === "off") {
+        dolight(false);
+    } else {
+        console.log("no action taken for POST")
+    }
+});
+
+app.post('/fan', function (req, res) {
+    console.log('received POST: %s', req.query)
+    var val = req.query.value;
+    if (val === "off") {
+        dofan(false, 0);
+    } else if (val === "on") {
+        var speed = req.query.speed;
+        switch (speed) {
+            case "1": dofan(true, 1); break;
+            case "2": dofan(true, 2); break;
+            case "3": dofan(true, 3); break;
+        } 
+    } else {
+        console.log("no action taken for POST")
+    }
+});
+
+dolight(false);
+dofan(false, 0);
+
+var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("HoodRelay app listening at http://%s:%s", host, port)
+ });
