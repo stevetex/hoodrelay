c7f73a8fde3165b38f5f3f6e4e01ec6f2abbaf4f
Steve Teixeira <steve@stevetex.com>
2019-02-02 15:52:16 -0800

    gpio stub and other changes

--- hoodrelay.js
+++ hoodrelay.js
@@ -1,4 +1,5 @@
-const gpio = require('onoff').Gpio;
+const GpioFactory = require('./gpiofactory');
+const gpio = GpioFactory.create();
 const express = require('express');
 var app = express();
 
@@ -8,7 +9,7 @@ const medfan = new gpio(22, 'out');
 const hifan = new gpio(25, 'out');
 
 function dolight(turnon) {
-    console.log('setting light to %s', turnon)
+    console.log('setting light to %s', turnon);
     if (turnon) {
         light.write(1);
     } else {
@@ -16,12 +17,12 @@ function dolight(turnon) {
     }
 }
 
-function dofan(turnon, speed) {
+function dofan(speed) {
     lofan.write(0);
     medfan.write(0);
     hifan.write(0);
-    if (turnon) {
-        console.log('setting fan to speed %d', speed)
+    if (speed > 0) {
+        console.log('setting fan to speed %d', speed);
         switch(speed) {
             case 1: lofan.write(1); break;
             case 2: medfan.write(1); break;
@@ -33,36 +34,45 @@ function dofan(turnon, speed) {
 }
 
 app.post('/light', function (req, res) {
-    console.log('received POST: %s', req.query)
+    console.log('received POST: %s', req.query);
     var val = req.query.value;
     if (val === "on") {
         dolight(true);
     } else if (val === "off") {
         dolight(false);
     } else {
-        console.log("no action taken for POST")
-    }
+        console.log("no action taken for POST");    }
 });
 
 app.post('/fan', function (req, res) {
-    console.log('received POST: %s', req.query)
-    var val = req.query.value;
-    if (val === "off") {
-        dofan(false, 0);
-    } else if (val === "on") {
-        var speed = req.query.speed;
-        switch (speed) {
-            case "1": dofan(true, 1); break;
-            case "2": dofan(true, 2); break;
-            case "3": dofan(true, 3); break;
-        } 
-    } else {
-        console.log("no action taken for POST")
-    }
+    console.log('received POST: %s', req.query);
+    var setvalue = 0;
+    var speed = req.query.speed;
+    switch (speed) {
+        case "1": setvalue = 1; break;
+        case "2": setvalue = 2; break;
+        case "3": setvalue = 3; break;
+    } 
+    dofan(setvalue);
+    console.log("Fan set to %s", setvalue);
+});
+
+app.get('/state', function (req, res) {
+    console.log('received GET: %s', req.query);
+    var fanval = 0; 
+    var lightval = light.read();
+    if (lofan.read() === 1) {
+        fanval = 1;
+    } else if (medfan.read() === 1) {
+        fanval = 2;
+    } else if (hifan.read() === 1) {
+        fanval = 3;
+    }        
+    res.json({light: lightval, fan: fanval});
 });
 
 dolight(false);
-dofan(false, 0);
+dofan(0);
 
 var server = app.listen(8081, function () {
     var host = server.address().address
