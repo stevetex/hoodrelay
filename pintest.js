3d17055f62b0ef47491728f54898801b4cf149f4
Steve Teixeira <steve@stevetex.com>
2019-02-24 12:25:20 -0800

    Adding ability to write 0 or 1 to pin

--- pintest.js
+++ pintest.js
@@ -12,8 +12,9 @@ app.use(function(req, res, next) {
 app.post('/pin', function (req, res) {
     console.log('received POST: %s', req.originalUrl);
     var val = req.query.value;
+    var mode = req.query.mode; // 0 or 1
     var pin = new gpio(val, 'out');
-    pin.writeSync(1);
+    pin.writeSync(Number(mode));
 //    pin.writeSync(0);
     res.status(200).send('pin written\n');
 });
